265064a2599b8a52304f291950c9b75244046c0a
Maxime Lombard <maxime.lombard@live.com.mx>
2022-08-08 22:15:53 +0200

    A few changes. Added a card instead of just the text.

--- src/About/TimelineTwoCardSingle.js
+++ src/About/TimelineTwoCardSingle.js
@@ -1,14 +1,39 @@
-import { aboutInfo } from "../JSON/DevInfo"
+// import { aboutInfo } from "../JSON/DevInfo"
 import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+// import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 const TimelineCard = ( {card} ) => {
   return (
     <Grid item xs={8} md={3}>
-      <div className="Timeline-card">
+      <Card sx={{ maxWidth: 345 }}>
+        {/* <CardMedia
+          component="img"
+          height="140"
+          image="/static/images/cards/contemplative-reptile.jpg"
+          alt="green iguana"
+        /> */}
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div" className="Timeline-title">{card.title}</Typography>
+          <Typography variant="h6" color={"text.secondary"} className="Timeline-occopation">{card.occupation}</Typography>
+          <Typography variant="body2" color="text.secondary" className="Timeline-description">
+            {card.description}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+      {/* <div className="Timeline-card">
         <h1 className="Timeline-title">{card.title}</h1>
         <h2 className="Timeline-ocupation">{card.occupation}</h2>
         <p className="Timeline-description" style={{color:"white"}}>{card.description}</p>
-      </div>
+      </div> */}
     </Grid>
   )
 }
