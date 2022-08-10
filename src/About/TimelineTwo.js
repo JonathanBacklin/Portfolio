ea775e70ee39d92870eb620df7f4b54d10044734
Maxime Lombard <maxime.lombard@live.com.mx>
2022-08-10 22:13:29 +0200

    Kind of complete timeline?

--- src/About/TimelineTwo.js
+++ src/About/TimelineTwo.js
@@ -35,7 +35,7 @@ const PrettoSlider = styled(Slider)({
   '& .MuiSlider-thumb': {
     height: 24,
     width: 24,
-    backgroundColor: '#fff',
+    backgroundColor: '#801228',
     border: '2px solid currentColor',
     '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
       boxShadow: 'inherit',
@@ -52,7 +52,7 @@ const PrettoSlider = styled(Slider)({
     width: 32,
     height: 32,
     borderRadius: '50% 50% 50% 0',
-    backgroundColor: '#52af77',
+    backgroundColor: '#f0204a',
     transformOrigin: 'bottom left',
     transform: 'translate(50%, -100%) rotate(-45deg) scale(0)',
     '&:before': { display: 'none' },
